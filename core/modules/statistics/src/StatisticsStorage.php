8e118202fe765b17373444a974340dc6837dcd4c
Tim Millwood <timmillwood@MacBook-Air.local>
2015-07-04 20:55:11 +0100

    adding statistics service

---
+++ core/modules/statistics/src/StatisticsStorage.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Drupal\statistics;
+
+use Drupal\Core\Database\Connection;
+
+class StatisticsStorage implements StatisticsStorageInterface {
+
+  /**
+   * The database connection used.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * Construct the statistics storage.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The database connection for the node view storage.
+   */
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function recordHit($nid) {
+    return (bool) $this->connection->merge('node_counter')
+      ->key('nid', $nid)
+      ->fields(array(
+        'daycount' => 1,
+        'totalcount' => 1,
+        'timestamp' => REQUEST_TIME,
+      ))
+      ->expression('daycount', 'daycount + 1')
+      ->expression('totalcount', 'totalcount + 1')
+      ->execute();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fetchViews($nid) {
+    if ($nid > 0) {
+      // Retrieve an array with both totalcount and daycount.
+      return $this->connection->select('node_counter', 'nc')
+        ->fields('nc', array('totalcount', 'daycount', 'timestamp'))
+        ->condition('nid', $nid, '=')->execute()->fetchAssoc();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clean($nid) {
+    return (bool) $this->connection->delete('node_counter')
+      ->condition('nid', $nid)
+      ->execute();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resetDayCount() {
+    return (bool) $this->connection->update('node_counter')
+      ->fields(array('daycount' => 0))
+      ->execute();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function maxTotalCount() {
+    $q = $this->connection->select('node_counter', 'nc');
+    $q->addExpression('MAX(totalcount)');
+    return (int) $q->execute()->fetchField();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function statisticsTitleList($dbfield, $dbrows) {
+  if (in_array($dbfield, array('totalcount', 'daycount', 'timestamp'))) {
+    $query = $this->connection->select('node_field_data', 'n');
+    $query->addTag('node_access');
+    $query->join('node_counter', 's', 'n.nid = s.nid');
+    $query->join('users_field_data', 'u', 'n.uid = u.uid');
+
+    return $query
+      ->fields('n', array('nid', 'title'))
+      ->fields('u', array('uid', 'name'))
+      ->condition($dbfield, 0, '<>')
+      ->condition('n.status', 1)
+      // @todo This should be actually filtering on the desired node status
+      //   field language and just fall back to the default language.
+      ->condition('n.default_langcode', 1)
+      ->condition('u.default_langcode', 1)
+      ->orderBy($dbfield, 'DESC')
+      ->range(0, $dbrows)
+      ->execute();
+  }
+  return FALSE;
+  }
+}
---
+++ core/modules/statistics/src/StatisticsStorageInterface.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\statistics\StatisticsStorageInterface.
+ */
+
+namespace Drupal\statistics;
+
+/**
+ * Provides an interface defining Statistics Storage
+ */
+interface StatisticsStorageInterface {
+
+  /**
+   * Returns if node view is counted
+   *
+   * @param int $nid
+   *   The id of the node to count
+   *
+   * @return bool
+   *   TRUE if the node view has been counted
+   */
+  public function recordHit($nid);
+  
+  /**
+   * Returns the number of times a node has been viewed
+   *
+   * @param int $nid
+   *   The id of the node to fetch the views for
+   *
+ * @return array
+ *   An associative array containing:
+ *   - totalcount: Integer for the total number of times the node has been
+ *     viewed.
+ *   - daycount: Integer for the total number of times the node has been viewed
+ *     "today". For the daycount to be reset, cron must be enabled.
+ *   - timestamp: Integer for the timestamp of when the node was last viewed.
+   */
+  public function fetchViews($nid);
+
+  /**
+   * Returns if node view counts are delete
+   *
+   * @param int $nid
+   *   The id of the node which views to delete
+   *
+   * @return bool
+   *   TRUE if the node views have been deleted
+   */
+  public function clean($nid);
+
+  /**
+   * Returns if day count is reset
+   *
+   * @return bool
+   *  TRUE if the day count is reset
+   */
+  public function resetDayCount();
+
+  /**
+   * Returns the highest 'totalcount' value
+   *
+   * @return int
+   *   The highest 'totalcount' value
+   */
+  public function maxTotalCount();
+
+  /**
+   * Returns the most viewed content of all time, today, or the last-viewed node.
+   *
+   * @param string $dbfield
+   *   The database field to use, one of:
+   *   - 'totalcount': Integer that shows the top viewed content of all time.
+   *   - 'daycount': Integer that shows the top viewed content for today.
+   *   - 'timestamp': Integer that shows only the last viewed node.
+   * @param int $dbrows
+   *   The number of rows to be returned.
+   *
+   * @return SelectQuery|FALSE
+   *   A query result containing the node ID, title, user ID that owns the node,
+   *   and the username for the selected node(s), or FALSE if the query could not
+   *   be executed correctly.
+   */
+  public function statisticsTitleList($dbfield, $dbrows);
+}
