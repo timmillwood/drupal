1be5c578a537dca2babe719d6e010969b169fcd5
Tim Millwood <timmillwood@MacBook-Air.local>
2015-07-07 15:37:21 +0100

    2524432-32

--- core/modules/system/src/SystemRequirements.php
+++ core/modules/system/src/SystemRequirements.php
@@ -13,17 +13,20 @@ namespace Drupal\system;
 class SystemRequirements {
 
   /**
-   * The minimum PHP version in the lower range
+   * The minimum PHP version in the lower range for the introduction
+   * of PDO::MYSQL_ATTR_MULTI_STATEMENTS.
    */
   public static $min_low = '5.5.21';
 
   /**
-   * The maximum PHP version in the lower range
+   * The maximum PHP version in the lower range for the introduction
+   * of PDO::MYSQL_ATTR_MULTI_STATEMENTS.
    */
   public static $max_low = '5.6.0';
 
   /**
-   * The minimum PHP version in the higher range
+   * The minimum PHP version in the higher range for the introduction
+   * of PDO::MYSQL_ATTR_MULTI_STATEMENTS.
    */
   public static $min_high = '5.6.5';
 
@@ -35,8 +38,6 @@ class SystemRequirements {
    * @return bool
    */
   public static function phpVersionWithPdoDisallowMultipleStatements($phpversion) {
-    // PDO::MYSQL_ATTR_MULTI_STATEMENTS was introduced in PHP versions 5.5.21
-    // and 5.6.5.
     return (version_compare($phpversion, self::$min_low, '>=') && version_compare($phpversion, self::$max_low, '<'))
       || version_compare($phpversion, self::$min_high, '>=');
   }
