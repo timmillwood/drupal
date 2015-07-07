b2fa45d42246e3e4c68ab7d99f90811a1a994c4f
Tim Millwood <timmillwood@MacBook-Air.local>
2015-07-07 13:15:56 +0100

    2524432-26

--- core/modules/system/src/SystemRequirements.php
+++ core/modules/system/src/SystemRequirements.php
@@ -11,6 +11,10 @@ namespace Drupal\system;
  * Class for helper methods used for the system requirements.
  */
 class SystemRequirements {
+  
+  public static $min_low = '5.5.21';
+  public static $max_low = '5.6.0';
+  public static $min_high = '5.6.22';
 
   /**
    * Determines whether the passed in PHP version disallows multiple statements.
@@ -22,8 +26,8 @@ class SystemRequirements {
   public static function phpVersionWithPdoDisallowMultipleStatements($phpversion) {
     // PDO::MYSQL_ATTR_MULTI_STATEMENTS was introduced in PHP versions 5.5.21
     // and 5.6.5.
-    return (version_compare($phpversion, '5.5.21', '>=') && version_compare($phpversion, '5.6.0', '<'))
-      || version_compare($phpversion, '5.6.5', '>=');
+    return (version_compare($phpversion, self::$min_low, '>=') && version_compare($phpversion, self::$max_low, '<'))
+      || version_compare($phpversion, self::$min_high, '>=');
   }
 
 }
