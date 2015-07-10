c561ae1de0c540166ca48c8518da5569cf208fb4
Tim Millwood <timmillwood@MacBook-Air.local>
2015-07-10 16:49:26 +0100

    1446932-7-

--- core/modules/statistics/src/StatisticsStorageInterface.php
+++ core/modules/statistics/src/StatisticsStorageInterface.php
@@ -15,74 +15,65 @@ namespace Drupal\statistics;
 interface StatisticsStorageInterface {
 
   /**
-   * Returns if node view is counted
+   * Count a node view.
    *
    * @param int $nid
-   *   The id of the node to count
+   *   The id of the node to count.
    *
    * @return bool
-   *   TRUE if the node view has been counted
+   *   TRUE if the node view has been counted.
    */
   public function recordHit($nid);
 
   /**
-   * Returns the number of times a node has been viewed
+   * Returns the number of times a node has been viewed.
    *
    * @param int $nid
-   *   The id of the node to fetch the views for
+   *   The id of the node to fetch the views for.
    *
- * @return array
- *   An associative array containing:
- *   - totalcount: Integer for the total number of times the node has been
- *     viewed.
- *   - daycount: Integer for the total number of times the node has been viewed
- *     "today". For the daycount to be reset, cron must be enabled.
- *   - timestamp: Integer for the timestamp of when the node was last viewed.
+   * @return array
+   *   An associative array containing:
+   *   - totalcount: Integer for the total number of times the node has been
+   *     viewed.
+   *   - daycount: Integer for the total number of times the node has been viewed
+   *     "today". For the daycount to be reset, cron must be enabled.
+   *   - timestamp: Integer for the timestamp of when the node was last viewed.
    */
   public function fetchViews($nid);
 
   /**
-   * Returns if node view counts are delete
+   * Delete counts for a specific node.
    *
    * @param int $nid
-   *   The id of the node which views to delete
+   *   The id of the node which views to delete.
    *
    * @return bool
-   *   TRUE if the node views have been deleted
+   *   TRUE if the node views have been deleted.
    */
   public function clean($nid);
 
   /**
-   * Returns if day count is reset
+   * Returns if reset is needed.
    *
    * @return bool
-   *  TRUE if the day count is reset
+   *  TRUE if reset is needed.
+   */
+  public function needsReset();
+
+  /**
+   * Resets the day count for all nodes.
+   *
+   * @return bool
+   *  TRUE if the day count is reset.
    */
   public function resetDayCount();
 
   /**
-   * Returns the highest 'totalcount' value
+   * Returns the highest 'totalcount' value.
    *
    * @return int
-   *   The highest 'totalcount' value
+   *   The highest 'totalcount' value.
    */
   public function maxTotalCount();
 
-  /**
-   * Returns the most viewed content of all time, today, or the last-viewed node.
-   *
-   * @param string $dbfield
-   *   The database field to use, one of:
-   *   - 'totalcount': Integer that shows the top viewed content of all time.
-   *   - 'daycount': Integer that shows the top viewed content for today.
-   *   - 'timestamp': Integer that shows only the last viewed node.
-   * @param int $dbrows
-   *   The number of rows to be returned.
-   *
-   * @return SelectQuery|FALSE
-   *   A query result containing the node ID, title, user ID that owns the node,
-   *   and the username for the selected node(s), or FALSE if the query could not
-   *   be executed correctly.
-   */
-  public function statisticsTitleList($dbfield, $dbrows);
 }
