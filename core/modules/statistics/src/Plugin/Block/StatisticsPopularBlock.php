41a1e9ba2ee349bee1d644fccdc7aff459380b93
Tim Millwood <timmillwood@MacBook-Air.local>
2015-07-11 09:51:22 +0100

    1446932

--- core/modules/statistics/src/Plugin/Block/StatisticsPopularBlock.php
+++ core/modules/statistics/src/Plugin/Block/StatisticsPopularBlock.php
@@ -11,8 +11,11 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Cache\Cache;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\statistics\StatisticsStorageInterface;
 
 /**
@@ -24,6 +27,13 @@ use Drupal\statistics\StatisticsStorageInterface;
  * )
  */
 class StatisticsPopularBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
 
   /**
    * The storage for statistics.
@@ -62,11 +72,14 @@ class StatisticsPopularBlock extends BlockBase implements ContainerFactoryPlugin
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager service.
    * @param \Drupal\statistics\StatisticsStorageInterface $statistics_storage
    *   The storage for statistics.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, StatisticsStorageInterface $statistics_storage) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, StatisticsStorageInterface $statistics_storage) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityManager = $entity_manager;
     $this->statisticsStorage = $statistics_storage;
   }
 
@@ -78,6 +91,7 @@ class StatisticsPopularBlock extends BlockBase implements ContainerFactoryPlugin
       $configuration,
       $plugin_id,
       $plugin_definition,
+      $container->get('entity.manager'),
       $container->get('statistics.statistics_storage')
     );
   }
@@ -101,15 +115,15 @@ class StatisticsPopularBlock extends BlockBase implements ContainerFactoryPlugin
     $access = AccessResult::allowedIfHasPermission($account, 'access content');
     if ($account->hasPermission('access content')) {
       $daytop = $this->configuration['top_day_num'];
-      if (!$daytop || !($result = $this->statisticsStorage->statisticsTitleList('daycount', $daytop)) || !($this->day_list = node_title_list($result, $this->t("Today's:")))) {
+      if (!$daytop || !($this->day_list = $this->statisticsStorage->fetchAll('daycount', $daytop))) {
         return AccessResult::forbidden()->inheritCacheability($access);
       }
       $alltimetop = $this->configuration['top_all_num'];
-      if (!$alltimetop || !($result = $this->statisticsStorage->statisticsTitleList('totalcount', $alltimetop)) || !($this->all_time_list = node_title_list($result, $this->t('All time:')))) {
+      if (!$alltimetop || !($this->all_time_list = $this->statisticsStorage->fetchAll('totalcount', $alltimetop))) {
         return AccessResult::forbidden()->inheritCacheability($access);
       }
       $lasttop = $this->configuration['top_last_num'];
-      if (!$lasttop || !($result = $this->statisticsStorage->statisticsTitleList('timestamp', $lasttop)) || !($this->last_list = node_title_list($result, $this->t('Last viewed:')))) {
+      if (!$lasttop || !($this->last_list = $this->statisticsStorage->fetchAll('timestamp', $lasttop))) {
         return AccessResult::forbidden()->inheritCacheability($access);
       }
       return $access;
@@ -164,21 +178,37 @@ class StatisticsPopularBlock extends BlockBase implements ContainerFactoryPlugin
     $content = array();
 
     if ($this->day_list) {
-      $content['top_day'] = $this->day_list;
+      $content['top_day'] = $this->nodeTitleList($this->day_list, $this->t("Today's:"));
       $content['top_day']['#suffix'] = '<br />';
     }
 
     if ($this->all_time_list) {
-      $content['top_all'] = $this->all_time_list;
+      $content['top_all'] = $this->nodeTitleList($this->all_time_list, $this->t("All time:"));
       $content['top_all']['#suffix'] = '<br />';
     }
 
     if ($this->last_list) {
-      $content['top_last'] = $this->last_list;
+      $content['top_last'] = $this->nodeTitleList($this->last_list, $this->t("Last viewed:"));
       $content['top_last']['#suffix'] = '<br />';
     }
 
     return $content;
+  }
+
+  protected function nodeTitleList($counts, $title){
+    $nodes = $this->entityManager->getStorage('node')->loadMultiple($counts);
+
+    $items = array();
+    foreach ($counts as $count) {
+      $items [] = \Drupal::l($nodes[$count]->getTitle(), new Url('entity.node.canonical', ['node' => $count], array()));
+    }
+
+    return array(
+      '#theme' => 'item_list__node',
+      '#items' => $items,
+      '#title' => $title,
+      '#cache' => ['tags' => Cache::mergeTags(['node_list'],Cache::buildTags('node', $counts))]
+      );
   }
 
 }
